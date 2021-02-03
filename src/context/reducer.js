c078947545fcdeed9eab4225413649a6e4688ec2
taurusilver7 <prasad20697@gmail.com>
2021-02-03 09:26:10 +0530

    action file updated

---
+++ src/context/reducer.js
@@ -0,0 +1,63 @@
+export const initialState = {
+  user: null,
+  playlists: [],
+  spotify: null,
+  top_artists: null,
+  playing: false,
+  discover_weekly: null,
+  item: null,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token,
+      };
+    case "SET_PLAYING":
+      return {
+        ...state,
+        playing: action.playing,
+      };
+
+    case "SET_ITEM":
+      return {
+        ...state,
+        item: action.item,
+      };
+
+    case "SET_PLAYLIST":
+      return {
+        ...state,
+        playlists: action.playlists,
+      };
+
+    case "SET_TOP_ARTISTS":
+      return {
+        ...state,
+        top_artists: action.top_artists,
+      };
+
+    case "SET_DISCOVER_WEEKLY":
+      return {
+        ...state,
+        discover_weekly: action.discover_weekly,
+      };
+    case "SET_SPOTIFY":
+      return {
+        ...state,
+        spotify: action.spotify,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
