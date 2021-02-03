fa31cf4b3f081267f93659d2256367f3861ce6a9
taurusilver7 <prasad20697@gmail.com>
2021-02-03 09:28:44 +0530

    player component updated

---
+++ src/components/Player/Player.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Player.css";
+
+import Sidebar from "../Sidebar/Sidebar";
+import Body from "../Body/Body";
+import Footer from "../Footer/Footer";
+
+const Player = ({ spotify }) => {
+  return (
+    <div className="player">
+      <div className="player__body">
+        <Sidebar />
+        <Body spotify={spotify} />
+        <Footer spotify={spotify} />
+      </div>
+    </div>
+  );
+};
+
+export default Player;
