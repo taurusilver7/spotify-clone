5d8b9d246df314295970c31ba4f869661e86a7b7
taurusilver7 <prasad20697@gmail.com>
2021-02-03 09:28:23 +0530

    header components updated & added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import "./Header.css";
+
+import SearchIcon from "@material-ui/icons/Search";
+import { Avatar } from "@material-ui/core";
+import { useDataLayerValue } from "../../context/DataLayerProvider";
+
+const Header = () => {
+  const [{ user }, dispatch] = useDataLayerValue();
+
+  return (
+    <div className="header">
+      <div className="header__left">
+        <SearchIcon />
+        <input placeholder="Search for Artists, Songs, Podcasts" type="text" />
+      </div>
+
+      <div className="header__right">
+        <Avatar src={user?.images[0]?.url} alt={user?.display_name} />
+        <h4>{user?.display_name}</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
