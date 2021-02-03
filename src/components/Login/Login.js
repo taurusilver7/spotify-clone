92983aca3692329f0b34c670400280100bf55bf7
taurusilver7 <prasad20697@gmail.com>
2021-02-03 09:30:13 +0530

    login page added with spotify auth

---
+++ src/components/Login/Login.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Login.css";
+import {loginURL} from '../../spotify';
+
+const Login = () => {
+  return (
+    <div className="login">
+      <img
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt="logo"
+      />
+      <a href={loginURL}>
+        Login with Spotify
+      </a>
+    </div>
+  );
+};
+
+export default Login;
