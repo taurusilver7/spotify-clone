c4c4ae6e63dca4c46b4d504a7f0357583dec0483
taurusilver7 <prasad20697@gmail.com>
2021-02-03 09:25:10 +0530

    hosting file updated

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAnuZhbJ2FLKMwrlGgH-RdKL3EC_03DtwY",
+  authDomain: "spotify-clone-e1c40.firebaseapp.com",
+  projectId: "spotify-clone-e1c40",
+  storageBucket: "spotify-clone-e1c40.appspot.com",
+  messagingSenderId: "996041075617",
+  appId: "1:996041075617:web:890bdca46bee8a262b79e0",
+  measurementId: "G-SHQPBWK6NG",
+};
